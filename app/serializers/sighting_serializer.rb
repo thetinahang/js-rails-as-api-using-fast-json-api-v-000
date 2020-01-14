a21ad4aae241aa447a5d83f60c724b0c50ff5f15
Tina Hang <mstinahang@gmail.com>
2020-01-14 03:13:11 +0000

    Automatically backed up by Learn

--- app/serializers/sighting_serializer.rb
+++ app/serializers/sighting_serializer.rb
@@ -1,6 +1,4 @@
 class SightingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :created_at
-  belongs_to :bird
-  belongs_to :location
+  attributes :created_at, :bird, :location
 end
